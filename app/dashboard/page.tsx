16183700fc0eead74df086ebcb366b28cc680236
Ashot <ashten@logixsoftware.com>
2025-03-05 19:35:09 +0400

    Fix: Build Error

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,23 +1,11 @@
 'use client'
 
-import { useSelector, useDispatch } from 'react-redux';
-import { RootState } from '../../redux/store';
-import { logout } from '../../redux/authSlice';
-
 export default function DashboardPage() {
-    const user = useSelector((state: RootState) => state.auth.user);
-    const dispatch = useDispatch();
 
-    const handleLogout = () => {
-        dispatch(logout());
-        // Redirect to login page
-        window.location.href = '/login';
-    };
 
     return (
         <div>
-            <h1>Welcome, {user}</h1>
-            <button onClick={handleLogout}>Logout</button>
+            <h1>Welcome</h1>
         </div>
     );
 }
