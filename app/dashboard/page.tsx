3c8ad5c01157f68aad77df6222be55f88cd76608
Ashot <ashten@logixsoftware.com>
2025-03-06 17:53:50 +0400

    local commit

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,22 @@
 'use client'
 
+import Link from "next/link";
+
 export default function DashboardPage() {
 
 
     return (
         <div>
             <h1>Welcome</h1>
+            <div className="checkbox_line mt-[26px]">
+              <label htmlFor="checkbox1">
+                <input type="checkbox" id="checkbox1" />
+                <span className="square_block"></span>
+                <span className="check_label">Show Password</span>
+              </label>
+            </div>
+            <button type="submit" className="login_submit">Login</button>
+            <Link href='/register'>Registr Now</Link>
         </div>
     );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex items-center justify-center h-full">
-       <div>Home Page</div>
+       <div>Home</div>
     </div>
   );
 }
